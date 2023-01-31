578a00e750eada337b7f495e1645d44fbf634d9d
Starinin Andrey <starinin-andrey@ya.ru>
2023-01-31 18:58:49 +0300

    Init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Старинин Андрей
+/*
+ * https://github.com/it-top-shambala/SplitArray.git
+ *
+ * Написать программу, которая из массива распределяет положительные, отрицательные и нулевые элементы в отдельные динамические массивы.
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
