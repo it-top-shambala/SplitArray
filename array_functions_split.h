5a9e788feaccbde2d4bccf7c97ae8b1ae16e1da1
Starinin Andrey <starinin-andrey@ya.ru>
2023-01-31 20:08:34 +0300

    Update

--- array_functions_split.h
+++ array_functions_split.h
@@ -32,3 +32,45 @@ int CalcZeros(int* arr, int size) {
 
     return zero;
 }
+
+int* CopyPositiveNumbers(int* arr, int size, int& new_size) {
+    new_size = CalcPositiveNumbers(arr, size);
+    int* positives = new int[new_size];
+
+    for (int i = 0, j = 0; i < size; ++i) {
+        if (arr[i] > 0) {
+            positives[j] = arr[i];
+            j++;
+        }
+    }
+
+    return positives;
+}
+
+int* CopyNegativeNumbers(int* arr, int size, int& new_size) {
+    new_size = CalcNegativeNumbers(arr, size);
+    int* negatives = new int[new_size];
+
+    for (int i = 0, j = 0; i < size; ++i) {
+        if (arr[i] < 0) {
+            negatives[j] = arr[i];
+            j++;
+        }
+    }
+
+    return negatives;
+}
+
+int* CopyZeros(int* arr, int size, int& new_size) {
+    new_size = CalcZeros(arr, size);
+    int* zeros = new int[new_size];
+
+    for (int i = 0, j = 0; i < size; ++i) {
+        if (arr[i] == 0) {
+            zeros[j] = arr[i];
+            j++;
+        }
+    }
+
+    return zeros;
+}
--- main.cpp
+++ main.cpp
@@ -6,8 +6,26 @@
  */
 
 #include <iostream>
+#include "array_functions_helpers.h"
+#include "array_functions_split.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const int SIZE = 10;
+    int source[SIZE];
+    RandomFillArray(source, SIZE, -5, 5);
+    PrintArray(source, SIZE);
+
+    int positive_size;
+    int* positives = CopyPositiveNumbers(source, SIZE, positive_size);
+    PrintArray(positives, positive_size);
+
+    int negative_size;
+    int* negatives = CopyNegativeNumbers(source, SIZE, negative_size);
+    PrintArray(negatives, negative_size);
+
+    int zeros_size;
+    int* zeros = CopyZeros(source, SIZE, zeros_size);
+    PrintArray(zeros, zeros_size);
+
     return 0;
 }
